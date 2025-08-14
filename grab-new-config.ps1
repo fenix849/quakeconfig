03ff0aece1773b8c695f9d899a56cee6da031dd8
Aaron Tate <fenix@f3n.org>
2025-08-14 22:19:36 +1000

    Config Update

--- grab-new-config.ps1
+++ grab-new-config.ps1
@@ -2,4 +2,5 @@ $steamid = '76561197963904034'
 $configname = 'fnx.cfg'
 Copy-Item -Force -Path "C:\Program Files (x86)\Steam\steamapps\common\Quake Live\$($steamid)\baseq3\$($configname)" -Destination $configname 
 Copy-Item -Force -Path "C:\Program Files (x86)\Steam\steamapps\common\Quake Live\$($steamid)\baseq3\ui" -Destination . -Recurse 
-git commit -am "Config Update"
+git commit -am "Config Update $(Get-Date.ToString('dd-MM-yyyy'))"
+git push origin main
